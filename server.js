580864bd6220cbf1888a5864dcc3398f9c60c08f
Wei Wu (COMMERCE) <weiw@ntdev.microsoft.com>
2017-10-25 09:10:11 +0800

    fix syntax error

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //a lightweight web service that returns random fortune cookie style 'i feel lucky' phrases
 //
 var http = require('http');
+var port = process.env.PORT || 8080;
 http.createServer(function(req, res) {
 				res.write("hello world");
 				res.end();
-			}).listen(process.env.PORT);
-});
+			}).listen(port);
+console.log("server waiting at http://localhost:" + port);
