4e94a1846098458e8091f1ad62f4f60557846c31
Vern Woo <vernwoo@gmail.com>
2020-02-18 22:55:58 +0800

    add isProd and isLiux switch

--- feelLucky.js
+++ feelLucky.js
@@ -2,24 +2,23 @@
 //
 var http = require('http');
 var fs = require('fs');
-
-process.on("uncaughtException", function(err){
-	fs.writeFile("Exception.txt", err.stack, "utf8");
-})
-
-var filePath = process.cwd() + "\\lucky.txt";
+var isProd = true;
+var portnum = isProd ? 80 : 8080 
+var isLinux = false;
+var file = isLinux? "/lucky.txt" : "\\lucky.txt";
+var filePath = process.cwd() + file;
 console.log("Reading from file " + filePath);
 var goodies = [];
 fs.readFile(filePath, function(err, data) { 
 			goodies= data.toString().split("\n");
 			console.log("Finish reading.");
 			var size = goodies.length;
-			var port = process.env.PORT || 8080;
+			var port = process.env.PORT || portnum;
 			http.createServer(function(req, res) {
 				index = Math.floor(Math.random()*size);
 				res.write(""+goodies[index]);
 				res.end();
 				console.log('index is ' + index + ':' + goodies[index]);
 			}).listen(port);
-			console.log("Now try your luck");
+			console.log("Now try your luck at http://localhost:" + portnum.toString());
 });
